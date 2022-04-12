1c9165d441de36e7311fdcd26c10e4d59d3e99ac
Danil Tolstov <85171174+DanilTolstov@users.noreply.github.com>
2022-04-12 15:03:46 +0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,21 @@
+const sliderControls = document.querySelectorAll(".slider-controls > button");
+const sliderContent = document.querySelectorAll(".features-list > li");
+if (sliderControls.length !== sliderContent.length) console.warn('Bad slider configuration')
 
-//обьявляем переменные
+function clearStyle(elements, style) {
+    elements.forEach(function (value, key) {
+        console.log(value, key)
+        value.classList.remove(style);
+    })
+}
 
-const slides = document.querySelectorAll('.feature-item');
-const controls = document.querySelectorAll('.slider-control');
+for (let i = 0; i < sliderControls.length; i++) {
+    sliderControls[i].addEventListener('click', function (evt) {
+        clearStyle(sliderControls, 'slider-control-active');
+        clearStyle(sliderContent, 'features-item-active');
+        sliderControls[i].classList.add('slider-control-active');
+        sliderContent[i].classList.add('features-item-active');
+    })
+}
 
 
